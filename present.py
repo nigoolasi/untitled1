7cfecbddfcf112e4466dc5d8aef91f4788263784
yanghy184 <yanghy184@chinaunicom.cn>
2018-07-10 17:38:09 +0800

    test

--- present.py
+++ present.py
@@ -3,16 +3,20 @@
 #
 import random
 import data
+import os
 
 max = 30
 pc = 0.8  # 交差概率
-selpc = 0.6#自然选择的概率
+selpc = 0.6  # 自然选择的概率
 Pm = 0.2  # 变异概率
-
+import os
 import logging
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-log = logging.getLogger(__name__)
+import logging.config
+filepath = os.path.join(os.path.dirname(__file__), 'log.conf')
+print(filepath)
+logging.config.fileConfig(filepath)
+log = logging.getLogger()
+l=logging.getLogger("resu")
 
 leb = 29
 
@@ -43,14 +47,10 @@ def calcutepower(R):
     map = data.map
     k1 = 0
     for r in R:
-        # log.info(r[0])
-        # log.info(r[1])
         k2 = map[r[0]][r[1]]
         e = k2 - k1
-        # print(e)
         car = car - cal(e)
         k1 = k2
-    # print("染色体的适应度：%s" % car)
     return car
 
 
@@ -58,17 +58,21 @@ def choose(p):
     population = []
     chroms = p
     chooselist = list(calcutepower(r) for r in p)
-    print("适应度列表：%s" % chooselist)
+    log.info("适应度列表：%s" % chooselist)
     chooseposible = ((q, w / sum(chooselist)) for q, w in enumerate(chooselist))
     chooseposible = list(chooseposible)
-    sorted(chooseposible, key=lambda x: x[1])
-    print("选择概率%s" % chooseposible)
+    chooseposible = sorted(chooseposible, key=lambda x: x[1], reverse=True)
+    chooselist = sorted(chooselist, reverse=True)
+    log.info("选择概率%s" % chooseposible)
+    log.info("选择%s" % chooselist)
+    l.info("适应度列表：%s" % chooselist)
     m = 0
     while m < len(chooseposible) * selpc:
+        log.info(chooseposible[m][0])
         population.append(chroms[chooseposible[m][0]])
         m = m + 1
-
-    log.info("自然选择出的个体数量：%s"%len(population))
+    log.info("自然选择出的个体数量：%s" % len(population))
+    log.info("选择出的个体的：%s" % population)
     return population
 
 
@@ -79,24 +83,25 @@ def crossgene(p):
     population = population + p
     inxe = len(p) * pc
     inxe = int(inxe)
-    print(inxe)
+    log.info("随机选择出个体进行%s次交配" % inxe)
     i = 0
+    childp = []
     while i < inxe:
         temp = []
         pos1 = random.randint(0, inxe) - 1  # 父亲
-        print("父亲染色体：%s" % pos1)
+        log.info("父亲染色体：%s" % pos1)
         pos2 = random.randint(0, inxe) - 1  # 母亲
-        print("母亲染色体：%s" % pos2)
+        log.info("母亲染色体：%s" % pos2)
         # 此处不再判断是不是相等，如果相等就按自我交配处处理
         w1 = random.randint(0, len(p[pos1]) - 1)
-        print("父亲基因交换位置：%s" % w1)
+        log.info("父亲基因交换位置：%s" % w1)
         w2 = random.randint(0, len(p[pos2]) - 1)
-        print("母亲基因交换位置：%s" % w2)
+        log.info("母亲基因交换位置：%s" % w2)
         ent1 = p[pos1]
         ent2 = p[pos2]
         log.info("父亲：%s" % ent1)
         log.info("母亲：%s" % ent2)
-        if ent1[w1][0] <= ent2[w2][0]:  # 取父亲的前面的基因
+        if ent1[w1][0] <= ent2[w2][0] and ent1[w1][1] <= ent2[w2][1]:  # 取父亲的前面的基因
             father = temp + list((w for q, w in enumerate(ent1) if q < w1))
             mather = list((w for q, w in enumerate(ent2) if q > w2))
             child = initanswer(temp, ent1[w1], ent2[w2])
@@ -104,7 +109,9 @@ def crossgene(p):
             log.info("来自母亲基因：%s" % mather)
             log.info("孩子自己的基因：%s" % child)
             child = father + child + mather
-        else:  ##取母亲的前面的基因
+            log.info("孩子：%s" % child)
+            childp.append(child)
+        elif ent1[w1][0] >= ent2[w2][0] and ent1[w1][1] >= ent2[w2][1]:  ##取母亲的前面的基因
             father = temp + list((w for q, w in enumerate(ent1) if q > w1))
             mather = list((w for q, w in enumerate(ent2) if q < w2))
             child = initanswer(temp, ent2[w2], ent1[w1])
@@ -112,16 +119,21 @@ def crossgene(p):
             log.info("来自母亲基因：%s" % mather)
             log.info("孩子自己的基因：%s" % child)
             child = mather + child + father
-        log.info("孩子：%s" % child)
-        population.append(child)
+            log.info("孩子：%s" % child)
+            childp.append(child)
+        else:
+            log.error("两个染色体虽然相爱，但是无法生孩子")
         i = i + 1
+    log.info("新产生的下一代个体数量%s" % len(childp))
+    log.info(childp)
+    population = population + childp
     log.info("交配后的种群中个体数量%s" % len(population))
-    print("交配后的种群%s" % population)
     return population
 
 
 # 开始变异。变异只能是行变到列变，这们的话，只后边的重新说计算
 def changenosignal(p):
+    log.info("变异前的种群中个体数量%s" % len(p))
     population = []
     pr = random.randint(1, 4)
     pm = pr * Pm
@@ -129,31 +141,35 @@ def changenosignal(p):
     for q in p:
         lk = lk + len(q)
     pos = lk * pm
-    print(pos)
+    log.info("计算在群体中要变异的位置：%s" % pos)
     genpos = divmod(pos, 2 * max - 2)[1]
     genpos = int(genpos)
     whichc = divmod(pos, 2 * max - 2)[0]
     whichc = int(whichc)
-    print("变异的点%s" % whichc)
-    print("变异的点%s" % genpos)
+    log.info("变异的染色体号：%s" % whichc)
+    log.info(p[whichc])
+    log.info("变异的基因位置：%s" % genpos)
     tem = p[whichc]
+    log.info("变异前的基因：%s" % str(tem[genpos]))
+    log.info(tem)
     k = genpos
     del tem[k + 1:]
-    log.info(tem)
     if tem[genpos][0] == tem[genpos - 1][0]:
         op = tem[genpos]
         del tem[genpos]
-        tem.append((op[0] + 1, op[1] - 1))
+        pl = ((op[0] + 1, op[1] - 1))
     else:
         op = tem[genpos]
         del tem[genpos]
-        tem.append((op[0] - 1, op[1] + 1))
+        pl = ((op[0] - 1, op[1] + 1))
+    log.info("变异后的基因：%s" % str(pl))
     u = []
-    u = initanswer(u, tem[genpos])
+    u = initanswer(u, pl)
     tem = tem + u
+    del p[whichc]
+    p.insert(whichc, tem)
     log.info(tem)
     population = population + p
-    # print("变异后的种群%s" % population)
     return population
     pass
 
@@ -161,13 +177,17 @@ def changenosignal(p):
 def begin():
     y = 0
     p = data.route
-    while y < 3:
-        print("第%s次迭代" % y)
+    while y < 51000:
+        log.info("第%s次迭代" % y)
         p = choose(p)
         p = crossgene(p)
         p = changenosignal(p)
-        p = choose(p)
-        print(p)
+        chooselist = list(calcutepower(r) for r in p)
+        log.info("适应度列表：%s" % chooselist)
+        chooseposible = ((q, w / sum(chooselist)) for q, w in enumerate(chooselist))
+        chooseposible = sorted(chooseposible, key=lambda x: x[1], reverse=True)
+        log.info("当前最好的路径：%s" % p[chooseposible[0][0]])
+        log.info("当前次好的路径：%s" % p[chooseposible[1][0]])
         y = y + 1
 
 
--- test.py
+++ test.py
@@ -1,32 +1,8 @@
-tem = [(0, 0), (1, 1), (3, 1), (4, 2), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12),
-       (14, 12), (15, 13), (15, 15), (15, 17), (16, 18), (16, 20), (17, 21), (19, 21), (21, 21), (21, 23), (22, 24),
-       (23, 25), (24, 26), (25, 27), (26, 28), (28, 28), (29, 29)]
-k = 6
-
-import random
-
-leb = 29
-
-
-def initanswer(result, item, ditem=(leb, leb)):
-    if item[0] >= ditem[0]:
-        if item[1] >= ditem[1]:
-            result.append(ditem)
-            return result
-    pos = random.randint(0, 1)
-    if pos == 0 and item[0] < ditem[0]:
-        result.append(item)
-        temp = (item[0] + 1, item[1])
-        return initanswer(result, temp, ditem)
-    elif pos == 1 and item[1] < ditem[1]:
-        result.append(item)
-        temp = (item[0], item[1] + 1)
-        return initanswer(result, temp, ditem)
-    return initanswer(result, item, ditem)
-
-
-result = []
-d = (9, 8)
-result = initanswer(result, (2, 3), ditem=d)
-print(result)
-print(len(result))
+import os
+import logging
+import logging.config
+filepath = os.path.join(os.path.dirname(__file__), 'log.conf')
+print(filepath)
+logging.config.fileConfig(filepath)
+log = logging.getLogger()
+log.info("test")
